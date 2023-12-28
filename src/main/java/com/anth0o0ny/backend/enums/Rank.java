3ffe3fedb57161571e34833d5ed2f9dc0cefa7a2
Anth0o0ny <pushkin.a.s.03@mail.ru>
2023-12-28 17:52:17 +0300

    update: rank enum structure

--- src/main/java/com/anth0o0ny/backend/enums/Rank.java
+++ src/main/java/com/anth0o0ny/backend/enums/Rank.java
@@ -24,6 +24,19 @@
 
 package com.anth0o0ny.backend.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Rank {
-    MS, CMS, I_A, II_A, III_A, I_Y, II_Y, III_Y, NR;
+    MS("МС"), CMS("КМС"), I_A("I"), II_A("II"), III_A("III"), I_Y("Iю"), II_Y("IIю"), III_Y("IIIю"), NR("Отсутствует");
+
+    private final String title;
+
+    Rank(String title) {
+        this.title = title;
+    }
+
+    @JsonValue
+    public String getTitle() {
+        return title;
+    }
 }
