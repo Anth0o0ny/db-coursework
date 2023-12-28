850db4022b855e5be028bdbd7e15c0b73b7a8ca5
Anth0o0ny <pushkin.a.s.03@mail.ru>
2023-12-29 02:43:26 +0300

    fix: package name

--- src/main/java/com/anth0o0ny/backend/service/ExerciseService.java
+++ src/main/java/com/anth0o0ny/backend/service/ExerciseService.java
@@ -1,4 +1,4 @@
-package com.anth0o0ny.backend.Service;
+package com.anth0o0ny.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
